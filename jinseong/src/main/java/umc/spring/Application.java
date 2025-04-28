b8fd72a57127bb2cd527f90e770da6320a3ecdc3
Jinseong <jinsung528@gmail.com>
2025-04-28 23:38:55 +0900

    feat : 5주차 시니어 미션 일부

--- jinseong/src/main/java/umc/spring/Application.java
+++ jinseong/src/main/java/umc/spring/Application.java
@@ -1,15 +1,28 @@
 package umc.spring;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import umc.spring.service.UserService.UserCommandService;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application {
+@RequiredArgsConstructor
+public class Application implements CommandLineRunner {
+
+	private final UserCommandService userCommandService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// UserService 호출
+		Long userId = 1L;
+		userCommandService.deleteUserWithRelatedEntities(userId);
+		System.out.println("삭제 완료");
+	}
 }
---
+++ jinseong/src/main/java/umc/spring/repository/UserRepository/UserRepository.java
@@ -0,0 +1,15 @@
+package umc.spring.repository.UserRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import umc.spring.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id = :userId")
+    void deleteUserById(@Param("userId") Long userId);
+}
