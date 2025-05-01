bbda4eae8ff0733c16125ceec3a47d1f1fcdba32
Jinseong <jinsung528@gmail.com>
2025-05-02 01:38:57 +0900

    feat : API 응답 통일 및 테스트 코드 작성

---
+++ jinseong/src/main/java/umc/spring/converter/TempConverter.java
@@ -0,0 +1,12 @@
+package umc.spring.converter;
+
+import umc.spring.web.dto.TempDTO.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
+}
