2a34bb59f219bebdb8ae990089a12fd6a09433be
mmije0ng <victon777@naver.com>
2025-05-03 23:37:04 +0900

    feat : 에러 핸들러 추가

--- mijeong/src/main/java/umc/spring/web/controller/TempRestController.java
+++ mijeong/src/main/java/umc/spring/web/controller/TempRestController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.TempConverter;
+import umc.spring.service.TempService.TempQueryService;
 import umc.spring.web.dto.TempResponse;
 
 @RestController
@@ -14,9 +16,17 @@ import umc.spring.web.dto.TempResponse;
 @RequiredArgsConstructor
 public class TempRestController {
 
+    private final TempQueryService tempQueryService;
+
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
 
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }
