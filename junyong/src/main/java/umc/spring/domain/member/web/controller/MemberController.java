ffe106945c58e724b559091abea432ee2bf65202
junyong <rlawnsdyd530@gmail.com>
2025-05-11 02:21:46 +0900

    setting : 8주차 - 스웨거 세팅

--- junyong/src/main/java/umc/spring/domain/member/web/controller/MemberController.java
+++ junyong/src/main/java/umc/spring/domain/member/web/controller/MemberController.java
@@ -2,24 +2,22 @@ package umc.spring.domain.member.web.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.domain.member.converter.MemberConverter;
 import umc.spring.domain.member.data.Member;
 import umc.spring.domain.member.service.MemberCommandService;
-import umc.spring.domain.member.service.MemberQueryService;
 import umc.spring.domain.member.web.dto.MemberRequestDTO;
 import umc.spring.domain.member.web.dto.MemberResponseDTO;
 import umc.spring.global.common.apiPayload.ApiResponse;
 
-@Controller
+@RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
 
+    @PostMapping("/")
     public ApiResponse<MemberResponseDTO.JoinResultDto> join(@RequestBody @Valid MemberRequestDTO.JoinDto request) {
         Member member = memberCommandService.joinMember(request);
         return ApiResponse.onSuccess(MemberConverter.toJoinResultDto(member));
